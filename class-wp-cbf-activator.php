7f232f76d493173a7b4238bb733fcc681ba0d635
Wajiha Rizvi <jiasweety@hotmail.com>
2016-11-11 09:00:33 -1000

    Add files via upload

---
+++ class-wp-cbf-activator.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wordpress.org  
+ * @since      1.0.0
+ *
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ */
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ * @author     Wajiha Rizvi  <jiyarizvi18@gmail.com>
+ */
+class Wp_Cbf_Activator {
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+	}
+}
