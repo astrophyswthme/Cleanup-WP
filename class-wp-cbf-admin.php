6d67469ab63b54dd15bd3ef254877abaff9de2d6
Wajiha Rizvi <jiasweety@hotmail.com>
2016-11-11 17:41:08 -1000

    Update class-wp-cbf-admin.php

--- class-wp-cbf-admin.php
+++ class-wp-cbf-admin.php
@@ -1,47 +1,49 @@
-public function enqueue_styles() {
+public function validate($input) {
+        // All checkboxes inputs
+        $valid = array();
 
-          /**
-           * This function is provided for demonstration purposes only.
-           *
-           * An instance of this class should be passed to the run() function
-           * defined in Wp_Cbf_Loader as all of the hooks are defined
-           * in that particular class.
-           *
-           * The Wp_Cbf_Loader will then create the relationship
-           * between the defined hooks and the functions defined in this
-           * class.
-         */ 
-            
-         if ( 'settings_page_wp-cbf' == get_current_screen() -> id ) {
-             // CSS stylesheet for Color Picker
-             wp_enqueue_style( 'wp-color-picker' );            
-             wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-cbf-admin.css', array( 'wp-color-picker' ), $this->version, 'all' );
-         }
+        //Cleanup
+        $valid['cleanup'] = (isset($input['cleanup']) && !empty($input['cleanup'])) ? 1 : 0;
+
+        $valid['comments_css_cleanup'] = (isset($input['comments_css_cleanup']) && !empty($input['comments_css_cleanup'])) ? 1: 0;
+
+        $valid['gallery_css_cleanup'] = (isset($input['gallery_css_cleanup']) && !empty($input['gallery_css_cleanup'])) ? 1 : 0;
+
+        $valid['body_class_slug'] = (isset($input['body_class_slug']) && !empty($input['body_class_slug'])) ? 1 : 0;
+
+        $valid['jquery_cdn'] = (isset($input['jquery_cdn']) && !empty($input['jquery_cdn'])) ? 1 : 0;
+
+        $valid['cdn_provider'] = esc_url($input['cdn_provider']);
+
+                // Login Customization
+                //First Color Picker
+                $valid['login_background_color'] = (isset($input['login_background_color']) && !empty($input['login_background_color'])) ? sanitize_text_field($input['login_background_color']) : '';
+
+                if ( !empty($valid['login_background_color']) && !preg_match( '/^#[a-f0-9]{6}$/i', $valid['login_background_color']  ) ) { // if user insert a HEX color with #
+                    add_settings_error(
+                            'login_background_color',                     // Setting title
+                            'login_background_color_texterror',            // Error ID
+                            'Please enter a valid hex value color',     // Error message
+                            'error'                         // Type of message
+                    );
+                }
+
+                //Second Color Picker
+                $valid['login_button_primary_color'] = (isset($input['login_button_primary_color']) && !empty($input['login_button_primary_color'])) ? sanitize_text_field($input['login_button_primary_color']) : '';
+                
+                if ( !empty($valid['login_button_primary_color']) && !preg_match( '/^#[a-f0-9]{6}$/i', $valid['login_button_primary_color']  ) ) { // if user insert a HEX color with #
+                    add_settings_error(
+                            'login_button_primary_color',                     // Setting title
+                            'login_button_primary_color_texterror',            // Error ID
+                            'Please enter a valid hex value color',     // Error message
+                            'error'                         // Type of message
+                    );
+                }
 
 
-    }
+                //Logo image id
+                $valid['login_logo_id'] = (isset($input['login_logo_id']) && !empty($input['login_logo_id'])) ? absint($input['login_logo_id']) : 0;
 
-    /**
-     * Register the JavaScript for the admin area.
-     *
-     * @since    1.0.0
-     */
-    public function enqueue_scripts() {
 
-        /**
-         * This function is provided for demonstration purposes only.
-         *
-         * An instance of this class should be passed to the run() function
-         * defined in Wp_Cbf_Loader as all of the hooks are defined
-         * in that particular class.
-         *
-         * The Wp_Cbf_Loader will then create the relationship
-         * between the defined hooks and the functions defined in this
-         * class.
-         */
-        if ( 'settings_page_wp-cbf' == get_current_screen() -> id ) {
-            wp_enqueue_media();   
-            wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-cbf-admin.js', array( 'jquery', 'wp-color-picker' ), $this->version, false );         
-        }
-
+        return $valid;
     }
